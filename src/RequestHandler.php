a97be4eda9963f4b3dd9de827249401cdc7fe87d
yosymfony <yosymfony@gmail.com>
2014-08-29 22:59:33 +0200

    Fix variable name from #2

--- src/RequestHandler.php
+++ src/RequestHandler.php
@@ -118,7 +118,7 @@ class RequestHandler implements RequestHandlerInterface
         if($result instanceof Response)
         {
             $this->statusCode = $result->getStatusCode();
-            $this->header = $result->headers->all();
+            $this->headers = $result->headers->all();
             $this->content = $result->getContent();
             
             return;
